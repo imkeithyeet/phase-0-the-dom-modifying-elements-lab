61d1cf9ab3919fd1533ceaed9d913af2c34b237f
Keith Ma <keithchezzy@gmail.com>
2022-11-02 16:18:30 -0700

    commit tree 90d9b316524bdf18328b0ac6bfaf00ae04a770d1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.querySelector('main');
+console.log('main')
+main.remove()
+
+const newHeader=document.createElement('h1');
+//console.log(newHeader)
+newHeader.id="victory";
+newHeader.innerHTML="Keith is the champion";
+document.body.append(newHeader)
+
